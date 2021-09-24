cc7384edf60401f5e554710dff94d1e4dd1aabac
fabian <macbook@macbooks-MacBook-Pro.local>
2021-09-24 15:39:32 +0200

    Added button to test our endpoint

--- src/App.js
+++ src/App.js
@@ -1,22 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { makeStyles } from "@mui/styles";
+import { Button } from "@mui/material";
+
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  providerContainer: {
+    marginTop: "50px",
+  },
+  buttonContainer: {
+    margin: "10px",
+    padding: "30px",
+  },
+  terraformLogo: {
+    marginRight: "50px",
+  },
+  logo: {
+    marginBottom: "50px",
+  },
+  success: {
+    color: "green",
+  },
+  error: {
+    color: "red",
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
+  const [endpointFetched, setEndpointFetched] = useState(false);
+  const [endpointResponded, setEndpointResponded] = useState(false);
+
+  const handleClick = () => {
+    const API_URL = process.env.REACT_APP_API_URL;
+    fetch(API_URL).then((response) => {
+      setEndpointResponded(response.status === 200);
+      setEndpointFetched(true);
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className={classes.container}>
+          <div>
+            <img
+              className={classes.logo}
+              width="300px"
+              src="assets/logo.svg"
+              alt="Terraform and AWS"
+            />
+          </div>
+          <div className={classes.buttonContainer}>
+            <Button onClick={handleClick} variant="contained">
+              Click me
+            </Button>
+          </div>
+          {endpointFetched && endpointResponded && (
+            <p className={classes.success}>SUCCESS!!!</p>
+          )}
+          {endpointFetched && !endpointResponded && (
+            <p className={classes.error}>API NOT READY!</p>
+          )}
+        </div>
+        <div className={classes.providerContainer}>
+          <img
+            className={classes.terraformLogo}
+            width="300px"
+            src="assets/terraform.png"
+            alt="Terraform and AWS"
+          />
+          <img width="300px" src="assets/aws.png" alt="Terraform and AWS" />
+        </div>
       </header>
     </div>
   );
